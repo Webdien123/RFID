c128b98b4c0f60be02a30cd489b650562625ad59
Tran Ly Van <vanb1305056@student.ctu.edu.vn>
2017-02-10 18:43:37 +0700

    no cmt

--- resource/RFID/resources/views/postForm.blade.php
+++ resource/RFID/resources/views/postForm.blade.php
@@ -1,5 +1,41 @@
-<form action="postForm" method="post">
-	{{ csrf_field() }}
-	<input type="text" name="HoTen">
-	<input type="submit">
-</form>
+<!DOCTYPE html>
+<html lang="vi">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container-fluid">
+			<h1 class="text-center">Hello World</h1>
+			<form action="postForm" method="post" id="myform">
+				{{ csrf_field() }}
+				<input type="text" name="HoTen" id="MaThe">
+				<input type="submit" 
+					style="position: absolute; left: -9999px; width: 1px; height: 1px;"
+					tabindex="-1" />
+			</form>
+		</div>
+		
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script type="text/javascript">
+			$('#myform').submit(function() {
+				$("#MaThe").val("Dolly Duck");
+			});
+		</script>
+	</body>
+</html>
